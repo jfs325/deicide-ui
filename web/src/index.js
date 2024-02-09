3c0c958c9b3c8551aa760ead33da8c56ad3644fb
Jason Lefever <jason.titus.lefever@gmail.com>
2024-02-09 15:05:41 -0500

    add dendrogram example

--- web/src/index.js
+++ web/src/index.js
@@ -1,15 +1,76 @@
-import { codeToHtml } from "shiki";
-import base64 from "base-64";
+import * as d3 from "d3";
+import * as data from "./flare.json";
 
-for (let ele of document.getElementsByClassName("sourcecode")) {
-    let sourcecode = ele.getAttribute("data-sourcecode");
-    let lang = ele.getAttribute("data-lang") || "java";
-    let theme = ele.getAttribute("data-theme") || "github-light";
+function chart() {
+    const width = 928;
 
-    if (sourcecode != null) {
-        codeToHtml(base64.decode(sourcecode), {
-            lang: lang,
-            theme: theme
-        }).then(html => ele.innerHTML = html);
-    }
+    // Compute the tree height; this approach will allow the height of the
+    // SVG to scale according to the breadth (width) of the tree layout.
+    const root = d3.hierarchy(data);
+    const dx = 10;
+    const dy = width / (root.height + 1);
+
+    // Create a tree layout.
+    const tree = d3.cluster().nodeSize([dx, dy]);
+
+    // Sort the tree and apply the layout.
+    root.sort((a, b) => d3.ascending(a.data.name, b.data.name));
+    tree(root);
+
+    // Compute the extent of the tree. Note that x and y are swapped here
+    // because in the tree layout, x is the breadth, but when displayed, the
+    // tree extends right rather than down.
+    let x0 = Infinity;
+    let x1 = -x0;
+    root.each(d => {
+        if (d.x > x1) x1 = d.x;
+        if (d.x < x0) x0 = d.x;
+    });
+
+    // Compute the adjusted height of the tree.
+    const height = x1 - x0 + dx * 2;
+
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-dy / 3, x0 - dx, width, height])
+        .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
+
+    const link = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 1.5)
+        .selectAll()
+        .data(root.links())
+        .join("path")
+        .attr("d", d3.linkHorizontal()
+            .x(d => d.y)
+            .y(d => d.x));
+
+    const node = svg.append("g")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-width", 3)
+        .selectAll()
+        .data(root.descendants())
+        .join("g")
+        .attr("transform", d => `translate(${d.y},${d.x})`);
+
+    node.append("circle")
+        .attr("fill", d => d.children ? "#555" : "#999")
+        .attr("r", 2.5);
+
+    node.append("text")
+        .attr("dy", "0.31em")
+        .attr("x", d => d.children ? -6 : 6)
+        .attr("text-anchor", d => d.children ? "end" : "start")
+        .text(d => d.data.name)
+        .clone(true).lower()
+        .attr("stroke", "white");
+
+    return svg.node();
 }
+
+console.log(chart());
+
+document.getElementById("chart").appendChild(chart());
